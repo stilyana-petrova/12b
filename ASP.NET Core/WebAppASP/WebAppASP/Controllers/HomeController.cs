4bdc840e834a12efa35ae34f8c14f2dfed32b7f5
stilyana-petrova <stilyanapetrova1@gmail.com>
2023-01-06 11:33:36 +0200

    Zadachi

---
+++ ASP.NET Core/WebAppASP/WebAppASP/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppASP.Models;
+
+namespace WebAppASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Message = "SOME MESSAGE HERE.";
+            return View();
+        }
+        public IActionResult Numbers(int count=5)
+        {
+            ViewBag.Count = count;
+            return View();
+        }
+        public IActionResult Files()
+        {
+            var path = @"C:\";
+            var files = Directory.GetDirectories(path).ToList();
+            files.AddRange(Directory.GetFiles(path));
+            return View(files);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
