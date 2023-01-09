b57bf5818e1107372d4bf0e36a02535670dc753f
stilyana-petrova <stilyanapetrova1@gmail.com>
2023-01-09 13:26:57 +0200

    Zadachi

---
+++ ASP.NET Core/CatGalery/CatGalery/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CatGalery.Models;
+
+namespace CatGalery.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<CatGalery.Models.Cat> Cats { get; set; }
+    }
+}
---
+++ ASP.NET Core/SchoolDiary/SchoolDiary/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SchoolDiary.Models;
+
+namespace SchoolDiary.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<SchoolDiary.Models.Student> Students { get; set; }
+
+    }
+}
